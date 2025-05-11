2b9efbbcda3e16f3468eb24a9393a9c924f1806c
JirakornChaitanaporn <jirakorn.c@ku.th>
2025-05-11 19:28:29 +0700

    debug main

--- main.py
+++ main.py
@@ -75,7 +75,8 @@ class RunGame:
         Welcome = pg.font.Font(None, 48).render("Welcome to Rogue-like chess", True, (255,255,255))
         play_text = pg.font.Font(None, 36).render("Press p to play", True, (255, 215, 0))
         guide_prompt = pg.font.Font(None, 24).render("Hold h to read guide book", True, (200, 200, 200))
-        sound_disable = pg.font.Font(None, 32).render("Press Q to disable sound", True, (65,105,255))
+        sound_disable = pg.font.Font(None, 32).render("Press Q to lower sound\n\
+                                                      Press E to turn up volume", True, (65,105,255))
         show_stat = pg.font.Font(None, 32).render("Press K to view statistic", True, (65,105,255))
         self.__screen.blit(guide_prompt, (285, 375))
         self.__screen.blit(play_text, (295, 250))
